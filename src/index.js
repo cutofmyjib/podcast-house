05da5dbb0a497287a5ba8e1e8a13459828f41642
cutofmyjib <thedianalozano@gmail.com>
2015-10-25 23:05:24 -0700

    import css file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import { App } from './App';
+import '../static/style.css';
 
-React.render(<App />, document.getElementById('root'));
+React.render(<App />, document.getElementById('site-name'));
