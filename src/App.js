52cac9b5c3d616e318542a8bf6301a801c10ecbb
cutofmyjib <thedianalozano@gmail.com>
2015-10-29 16:57:42 -0700

    move playlists component to a separate file and import into App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Playlist } from './playlists';
 
 class Header extends Component {
   render() {
@@ -46,21 +47,6 @@ class PlaylistContainer extends Component {
     return (
       <div className="playlists">
         {podcastPlaylists}
-      </div>
-    );
-  }
-}
-
-class Playlist extends Component {
-  render() {
-    return (
-      <div className="playlist">
-        <img src={this.props.img} />
-        <div className="playlist-content">
-          <p className="playlist-duration">{this.props.duration}</p>
-          <h1 className="playlist-title">{this.props.title}</h1>
-          <p className="playlist-desc">{this.props.description}</p>
-        </div>
       </div>
     );
   }
---
+++ src/playlists.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+module.exports = class Playlist extends Component {
+  render() {
+    return (
+      <div className="playlist">
+        <img src={this.props.img} />
+        <div className="playlist-content">
+          <p className="playlist-duration">{this.props.duration}</p>
+          <h1 className="playlist-title">{this.props.title}</h1>
+          <p className="playlist-desc">{this.props.description}</p>
+        </div>
+      </div>
+    );
+  }
+}
