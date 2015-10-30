f301b7c3aa8b05fa1561f91183103d403ccdad02
cutofmyjib <thedianalozano@gmail.com>
2015-10-29 17:01:01 -0700

    use export instead of module.exports

--- src/playlists.js
+++ src/playlists.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-module.exports = class Playlist extends Component {
+export class Playlist extends Component {
   render() {
     return (
       <div className="playlist">
