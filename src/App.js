beccbf01b7ad1a3bd0cd8dd9bd054356e6c0d7ed
cutofmyjib <thedianalozano@gmail.com>
2015-10-30 22:33:21 -0700

    change content and background photos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Header } from './header';
-import { Featured } from './featured';
-import { PlaylistContainer } from './playlist-container';
-import { Playlist } from './playlists';
-import { Footer } from './footer';
+import Header from './header';
+import Featured from './featured';
+import PlaylistContainer from './playlist-container';
+import Playlist from './playlists';
+import Footer from './footer';
 
 export class App extends Component {
   render() {
@@ -14,24 +14,24 @@ export class App extends Component {
       subscription: "my subscription"
     };
     const featuredProps = {
-      img: "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg",
+      img: "https://snap-photos.s3.amazonaws.com/img-thumbs/960w/E7CD4AAEF3.jpg",
       duration: "1h 30m",
       title: "Featured Podcast Playlist Title",
       desc: "Austin viral whatever, mlkshk Odd Future 3 wolf moon squid aesthetic ugh beard butcher. Crucifix master cleanse viral jean shorts Banksy PBR selfies. Dreamcatcher scenester pickled chambray kitsch. Next level distillery you probably haven't heard of them tattooed. IPhone tofu occupy next level.",
       button: "Check it out"
     };
     const playlistProps = [{
-      img: "http://i3.kym-cdn.com/photos/images/original/000/581/251/5af.jpg",
+      img: "https://snap-photos.s3.amazonaws.com/img-thumbs/960w/27PNUR3Z83.jpg",
       duration: "1h 24m",
       title: "Podcast Playlist Title",
       description: "Rub face on owner shove bum in owner's face like camera lens. Why must they do that. Sits on keyboard, spit up on light gray carpet instead of adjacent linoleum destroy couch as revenge for chew iPad power cord chew iPad power cord chase dog then run away. Hide from vacuum cleaner chew on cable, and intently sniff hand, or see owner, run in terror, chase dog then run away for cough furball but hiss at vacuum cleaner."
     }, {
-      img: "http://img.ifcdn.com/images/5aa617f17978baf23e992f1e86c038627c11d9a6300fabc7b939d09508a273e0_1.jpg",
+      img: "https://snap-photos.s3.amazonaws.com/img-thumbs/960w/5LXBN8H2CQ.jpg",
       duration: "1h 24m",
       title: "Podcast Playlist Title",
       description: "Rub face on owner shove bum in owner's face like camera lens run in terror, chase dog then run away for cough furball but hiss at vacuum cleaner."
     }, {
-      img: "http://i0.kym-cdn.com/photos/images/facebook/000/733/489/6c3.jpg",
+      img: "https://snap-photos.s3.amazonaws.com/img-thumbs/960w/J6P5U7BJ9T.jpg",
       duration: "1h 24m",
       title: "Podcast Playlist Title",
       description: "Rub face on owner shove bum in owner's face like camera lens. Why must they do that. Hide from vacuum cleaner chew on cable, and intently sniff hand, or see owner, run in terror, chase dog then run away for cough furball but hiss at vacuum cleaner."
