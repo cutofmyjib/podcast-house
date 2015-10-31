427dc1ff024015fbba639e81b5eb7ceb8d5c7fce
cutofmyjib <thedianalozano@gmail.com>
2015-10-31 02:29:13 -0700

    add PlaylistArticle route

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import App from './App';
 import Home from './home';
 import Featured from './featured';
 import FeaturedArticle from './featured-article';
+import PlaylistArticle from './playlist-article';
 
 let routes = (
   <Route name='app' path='/' handler={App} >
     <Route name='home' path='/' handler={Home} />
     <Route name='featured' path='/featured' handler={FeaturedArticle} />
+    <Route name='playlist' path='/playlist' handler={PlaylistArticle} />
   </Route>
 );
 
