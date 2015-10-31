ff456f5d42faa1e550569915a9f18ab856ace6e3
cutofmyjib <thedianalozano@gmail.com>
2015-10-31 01:56:13 -0700

    take out handleclick and add link router to button

---
+++ src/featured-article.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Router from 'react-router';
+import { Link, RouteHandler } from 'react-router';
+
+export default class FeaturedArticle extends Component {
+  render() {
+    return (
+      <div><h1>FEATURED</h1></div>
+    );
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
+import App from './App';
+import Featured from './featured';
+import FeaturedArticle from './featured-article';
+
+let routes = (
+  <Route name='home' path='/' handler={App} >
+    <Route name='featured' path='/featured' handler={FeaturedArticle} />
+  </Route>
+);
+
+export default routes;
