3ccc07d5fa9a9fa166c859a5638c9a0e38988f13
cutofmyjib <thedianalozano@gmail.com>
2015-10-31 02:30:19 -0700

    add FPO js for playlist article

---
+++ src/playlist-article.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PlaylistArticle extends Component {
+  render() {
+    return (
+      <div><h1>PLAYLIST ARTICLE HERE</h1></div>
+    );
+  }
+}
