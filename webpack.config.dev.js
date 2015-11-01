e1ecdb8a0b8d2ba307aa5e08dc4bb9ca8e866fe1
cutofmyjib <thedianalozano@gmail.com>
2015-11-01 01:05:29 -0700

    add loader for png

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -27,6 +27,12 @@ module.exports = {
     {
       test: /\.css$/,
       loader: 'style-loader!css-loader'
+    },
+    // { test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+    //   loader: 'url-loader?limit=100000'
+    // }
+    { test: /\.(png|jpg)$/,
+      loader: 'file-loader?name=[name].[ext]'
     }
     ]
   }
