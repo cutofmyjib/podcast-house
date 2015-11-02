af18413903b201add032352e9439796db81dbce2
cutofmyjib <thedianalozano@gmail.com>
2015-11-01 22:11:12 -0800

    import share icons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Router from 'react-router';
 import '../static/style.css';
 import '../static/style-playlist-article.css';
+import '../static/twitter-icon.png';
+import '../static/facebook-icon.png';
+import '../static/linkedin-icon.png';
+import '../static/email-icon.png';
 import routes from './routes';
 
 Router.run(routes, Router.HistoryLocation, (Handler) => {
