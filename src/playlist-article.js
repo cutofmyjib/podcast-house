13808549ee1a06b08b2a33bdeabf1fff2bb747f0
cutofmyjib <thedianalozano@gmail.com>
2015-11-02 22:10:53 -0800

    add playlist article elements

--- src/playlist-article.js
+++ src/playlist-article.js
@@ -7,19 +7,22 @@ export default class PlaylistArticle extends Component {
         <div className="podlist">
           <div className="background-tamer">
           </div>
-          <div className="podlist-content">
-            <h1 className="podlist-title">Featured Content Here</h1>
-            <audio controls></audio>
+          <div className="podlist-content-containter">
+            <div className="podlist-content">
+              <h1 className="podlist-title">Featured Content Here</h1>
+              <div className="audio"><audio controls></audio></div>
+            </div>
           </div>
         </div>
-          <div className="share-playlist">
-            <ul className="share-icons">
-              <li><a href="#"><img src="/static/twitter-icon.png" /></a></li>
-              <li><a href="#"><img src="/static/facebook-icon.png" /></a></li>
-              <li><a href="#"><img src="/static/linkedin-icon.png" /></a></li>
-              <li><a href="#"><img src="/static/email-icon.png" /></a></li>
-            </ul>
-          </div>
+
+        <div className="share-playlist">
+          <ul className="share-icons">
+            <li><a href="#"><img src="/static/twitter-icon.png" /></a></li>
+            <li><a href="#"><img src="/static/facebook-icon.png" /></a></li>
+            <li><a href="#"><img src="/static/linkedin-icon.png" /></a></li>
+            <li><a href="#"><img src="/static/email-icon.png" /></a></li>
+          </ul>
+        </div>
       </div>
     );
   }
