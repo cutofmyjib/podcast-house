006ebf5a810e41e88900ad90293b1fb9a86ca27c
cutofmyjib <thedianalozano@gmail.com>
2015-11-05 11:14:48 -0800

    build contact component

---
+++ src/contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <div className="contact-container">
+          <div id="contact-form"></div>
+        </div>
+      </div>
+    );
+  }
+}
