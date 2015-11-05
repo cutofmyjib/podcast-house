ebef1f7178161d945ce333cf76275f8da8a39ba5
cutofmyjib <thedianalozano@gmail.com>
2015-11-05 12:04:03 -0800

    initial commit for contact form

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 export default class Contact extends Component {
+  propTypes: {
+    value: React.PropTypes.object.isRequired,
+    onChange: React.PropTypes.func.isRequired
+  }
+
   render() {
     return (
-      <div>
-        <div className="contact-container">
-          <div id="contact-form"></div>
-        </div>
-      </div>
+      React.createElement('form', {className:'Contact'},
+          React.createElement('input', {
+            type: 'text',
+            placeholder: 'Name (required)',
+            value: this.props.value.name,
+          }),
+          React.createElement('input', {
+            type: 'email',
+            placeholder: 'Email',
+            value: this.props.value.email,
+          }),
+          React.createElement('textarea', {
+            placeholder: 'Message',
+            value: this.props.value.message,
+          }),
+          React.createElement('button', {
+            type: 'submit'
+          }, "Send")
+          )
     );
   }
 }
