6abbb9231b6bb61a676bf3c1f36bf645dd0bdac5
Claudiu Andrei <claudiu.andrei@gmail.com>
2015-11-05 12:14:57 -0800

    This should be the same using JSX

--- src/contact.js
+++ src/contact.js
@@ -8,25 +8,12 @@ export default class Contact extends Component {
 
   render() {
     return (
-      React.createElement('form', {className:'Contact'},
-          React.createElement('input', {
-            type: 'text',
-            placeholder: 'Name (required)',
-            value: this.props.value.name,
-          }),
-          React.createElement('input', {
-            type: 'email',
-            placeholder: 'Email',
-            value: this.props.value.email,
-          }),
-          React.createElement('textarea', {
-            placeholder: 'Message',
-            value: this.props.value.message,
-          }),
-          React.createElement('button', {
-            type: 'submit'
-          }, "Send")
-          )
-    );
+      <form className="contact">
+        <input type="text" placeholder="Name (required)" value={this.props.value.name} />
+        <input type="email" placeholder="Email" value={this.props.value.email} />
+        <textarea placeholder="Message" value={this.props.value.message} />
+        <button type="submit">Send</button>
+      </form>
+    )
   }
 }
