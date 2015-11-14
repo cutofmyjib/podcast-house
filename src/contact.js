bd0cb28bcd1181806252f4a5f2607c5472a32a15
cutofmyjib <thedianalozano@gmail.com>
2015-11-13 23:18:34 -0800

    place google static map

--- src/contact.js
+++ src/contact.js
@@ -5,11 +5,16 @@ export default class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        <div className="contact-info">
-          <h4 className="contact-info-header">San Francisco HQ</h4>
-          <p>108 Newsite Street</p>
-          <p>San Francisco, CA</p>
-          <p>94105</p>
+        <div>
+          <img id="map" src="https://maps.googleapis.com/maps/api/staticmap?center=South+Beach,San+Francisco,CA&zoom=13&size=600x300&maptype=roadmap
+&markers=color:red%7Clabel:podcasthouse%7C37.786355,-122.391199
+&key=AIzaSyAJzQKtYZxDKxdZ16O_jYJCznIvMiyx_1k" />
+          <div className="contact-info">
+            <h4 className="contact-info-header">San Francisco HQ</h4>
+            <p>108 Newsite Street</p>
+            <p>San Francisco, CA</p>
+            <p>94105</p>
+          </div>
         </div>
         <div className="contact-form">
           <form>
