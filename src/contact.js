fa4c5e48bb7c082b5f2b8b7475d7b8ea219b8515
cutofmyjib <thedianalozano@gmail.com>
2015-11-13 23:29:23 -0800

    organize div containers

--- src/contact.js
+++ src/contact.js
@@ -5,11 +5,11 @@ export default class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        <div>
-          <img id="map" src="https://maps.googleapis.com/maps/api/staticmap?center=South+Beach,San+Francisco,CA&zoom=13&size=600x300&maptype=roadmap
+        <div className="contact-info">
+          <img id="map" src="https://maps.googleapis.com/maps/api/staticmap?center=South+Beach,San+Francisco,CA&zoom=13&size=300x300&maptype=roadmap
 &markers=color:red%7Clabel:podcasthouse%7C37.786355,-122.391199
 &key=AIzaSyAJzQKtYZxDKxdZ16O_jYJCznIvMiyx_1k" />
-          <div className="contact-info">
+          <div>
             <h4 className="contact-info-header">San Francisco HQ</h4>
             <p>108 Newsite Street</p>
             <p>San Francisco, CA</p>
